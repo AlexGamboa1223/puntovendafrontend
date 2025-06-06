c599fafb6fbf25c04c821743474e9cd6480438f2
AlexGamboa1223 <L21450180@itz.edu.mx>
2025-06-05 18:06:44 -0600

    Corrijo ruta en vite-static-copy para Render

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,23 @@
-import path from "path"
-import tailwindcss from "@tailwindcss/vite"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
+import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'public/_redirects',
+          dest: '.' // Copia al root del dist
+        }
+      ]
+    })
+  ],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
-  server: {
-    https: {
-      key: 'mypuntoventaapp-privateKey.key',
-      cert:'mypuntoventaapp.crt'
-    }
-  }
 })
